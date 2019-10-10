80ca53777fd7d6352a1ea8c634a02d9013dfd933
duy <duyme18@gmail.com>
2019-10-10 09:33:01 +0700

    add project

---
+++ src/main/java/com/hdd/configuration/AppInializer.java
@@ -0,0 +1,23 @@
+package com.hdd.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { ApplicationConfiguration.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+}
+
---
+++ src/main/java/com/hdd/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.hdd.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
